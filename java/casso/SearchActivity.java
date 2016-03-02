563b5ffd791ed069c635e08c32bfcecee9ba6c83
stephrhee <srhee93@gmail.com>
2016-03-01 22:36:08 -0500

    search screen keyboard input flow

--- java/casso/SearchActivity.java
+++ java/casso/SearchActivity.java
@@ -3,19 +3,22 @@ package casso;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.Window;
-import android.view.WindowManager;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
 
+import android.widget.TextView;
 import com.casso.R;
+
+import java.security.KeyException;
 
 public class SearchActivity extends FragmentActivity {
 
-    private EditText mArtistNameSearchBox;
-    private EditText mPieceTitleSearchBox;
-    private Button mSearchButton;
+    private EditText mArtistNameSearchField;
+    private EditText mArtworkTitleSearchField;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -27,15 +30,20 @@ public class SearchActivity extends FragmentActivity {
     }
 
     private void init() {
-        mArtistNameSearchBox = (EditText) findViewById(R.id.search_artist_name_edit_text);
-        mPieceTitleSearchBox = (EditText) findViewById(R.id.search_piece_title_edit_text);
-        mSearchButton = (Button) findViewById(R.id.search_button);
-        mArtistNameSearchBox.setHint(R.string.artist_name);
-        mPieceTitleSearchBox.setHint(R.string.piece_title);
-        mSearchButton.setOnClickListener(new View.OnClickListener() {
+        mArtistNameSearchField = (EditText) findViewById(R.id.search_artist_search_field);
+        mArtworkTitleSearchField = (EditText) findViewById(R.id.search_artwork_title_search_field);
+
+        mArtistNameSearchField.setHint(R.string.search_artist_name_question_string);
+        mArtworkTitleSearchField.setHint(R.string.search_artwork_title_question_string);
+        mArtworkTitleSearchField.setImeOptions(EditorInfo.IME_ACTION_SEARCH);
+        mArtworkTitleSearchField.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
-            public void onClick(View v) {
-                doSearch();
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                    doSearch();
+                    return true;
+                }
+                return false;
             }
         });
     }
