f47268f49f299c2751a3f08ea906a6ed4fdb70b0
stephrhee <srhee93@gmail.com>
2016-04-14 01:14:40 -0400

    add SquareImageView

---
+++ java/casso/widget/SquareImageView.java
@@ -0,0 +1,27 @@
+package casso.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+public class SquareImageView extends ImageView {
+
+    public SquareImageView(Context context) {
+        super(context);
+    }
+
+    public SquareImageView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public SquareImageView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int dim = getMeasuredWidth() < getMeasuredHeight() ? getMeasuredWidth() : getMeasuredHeight();
+        setMeasuredDimension(dim, dim);
+    }
+}
